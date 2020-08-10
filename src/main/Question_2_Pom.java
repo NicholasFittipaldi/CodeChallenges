63d187554c9e5c4ea429cb7c1d7f40d09dd3edd2
Nicholas Fittipaldi <nfittutc@gmail.com>
2020-08-10 12:33:50 -0400

    Added and completed question 3

--- src/main/Question_2_Pom.java
+++ src/main/Question_2_Pom.java
@@ -2,9 +2,6 @@ package main;
 
 public class Question_2_Pom {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 }
---
+++ src/main/Question_3.java
@@ -0,0 +1,37 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Question_3 {
+	
+	public static void printSmallestNumber(ArrayList<Integer> array, int index) {
+		Collections.sort(array);
+		System.out.println("Smallest number at position " + (index + 1) 
+				+ ": " + array.get(index));
+	}
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		ArrayList<Integer> array = new ArrayList<>();
+		
+		
+		System.out.println("Please enter a number between 1 and 500.");
+		Scanner scan = new Scanner(System.in);
+		int index = scan.nextInt();
+		
+		if (index > 500 || index < 1)
+			System.out.println("You have entered a number outside the range of 1 to 500, "
+					+ "\nplease enter a different number.");
+		else {
+			for (int i = 1; i <= 500; i++) 
+				array.add(random.nextInt());
+			
+			printSmallestNumber(array, index - 1);
+		}
+		
+		scan.close();
+	}
+}
